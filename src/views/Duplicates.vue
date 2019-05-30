360cfc082d5752a3224f8c39988f0e167b31d1b2
EUGENE <RUBEL.EUGENE@GMAIL.COM>
2019-05-30 10:55:55 +0300

    2.0

---
+++ src/views/Duplicates.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "Duplicates"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="users">
+    <v-toolbar flat>
+      <v-toolbar-title>PROJECTS</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" dark @click="">
+        add new project
+      </v-btn>
+    </v-toolbar>
+
+    <v-data-table
+            :headers="headers"
+            :items="users"
+            class="elevation-1">
+      <template v-slot:items="props">
+        <td>{{ props.item.id }}</td>
+        <td class="text-xs-left">{{ props.item.projectName }}</td>
+        <td class="text-xs-left">{{ props.item.projectDescription }}</td>
+        <td class="text-xs-left">{{ props.item.projectLanguage }}</td>
+        <td class="text-xs-left">{{ props.item.projectActive }}</td>
+        <td class="text-xs-left"><v-btn dark color="#256fab">Statistics</v-btn></td>
+        <td class="text-xs-left"><v-btn dark color="#256fab">Search</v-btn></td>
+        <td class="text-xs-left"><v-btn dark color="#256fab">Duplicate</v-btn></td>
+        <td class="text-xs-left"><v-btn dark color="#256fab">Registration</v-btn></td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      users: [],
+      headers: [
+        {text: 'ID', value: '#',sortable: false,},
+        {text: 'NAME', value: '#',sortable: false,},
+        {text: 'DESCRIPTION', value: '#',sortable: false,},
+        {text: 'LANGUAGE', value: '#',sortable: false,},
+        {text: 'ACTIVE', value: '#',sortable: false,},
+        {text: 'STATISTICS', value: '#',sortable: false,},
+        {text: 'SEARCH', value: '#',sortable: false,},
+        {text: 'DUPLICATE', value: '#',sortable: false,},
+        {text: 'REGISTRATION', value: '#',sortable: false,},
+      ]
+    }
+  },
+  created() {
+    this.axios.get('http://192.168.0.143:9000/api/v1/admin/project', {
+      headers : {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('access_token'),
+      }
+    })
+      .then(response => this.users = response.data.data)
+      .catch(()=>{})
+  }
+}
+</script>
+
+<style>
+  td {
+    width: 200px;
+    height: 50px;
+  }
+</style>
